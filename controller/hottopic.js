a00ce2351256c8d475d65a5f3d13578dc9789206
jayseo916 <jayseo916@gmail.com>
2019-09-06 16:33:35 +0900

    코드단축

--- controller/hottopic.js
+++ controller/hottopic.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-const hotTopicSchema = new mongoose.Schema({
-  articleList: [String],
-  count: Number,
-  topic: String,
-  url: String,
-  text: String
-});
+const HotTopic = mongoose.model(
+  'hottopic',
+  new mongoose.Schema({
+    articleList: [String],
+    count: Number,
+    topic: String,
+    url: String,
+    text: String
+  })
+);
 
-const HotTopic = mongoose.model('hottopic', hotTopicSchema);
+//최대 다섯개 의 핫토픽
+//각 카테고리별 탑 7개 선별 후 배치
